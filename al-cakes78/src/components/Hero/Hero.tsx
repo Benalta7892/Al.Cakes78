bb7d55d8309d7f1488b232c9ae9e8c32b862ffa9
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-05-30 00:21:53 +0200

    feat(hero): add navigation links to Gâteaux and Contact pages

--- al-cakes78/src/components/Hero/Hero.tsx
+++ al-cakes78/src/components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/Hero.module.css";
 import HeroCarousel from "./HeroCarousel";
 import { EmblaOptionsType } from "embla-carousel";
+import Link from "next/link";
 
 const OPTIONS: EmblaOptionsType = { loop: true };
 // const SLIDE_COUNT = 3;
@@ -19,10 +20,12 @@ const Hero = () => {
       <div className={styles.heroContent}>
         <h1 className={styles.heroTitle}>Entremets pour vos Évènements</h1>
         <div className={styles.heroButtons}>
-          <button className={styles.primaryBtn}>Découvrir</button>
-          <button className={styles.secondaryBtn}>
+          <Link href="/gateaux" className={styles.primaryBtn}>
+            Découvrir
+          </Link>
+          <Link href="/contact" className={styles.secondaryBtn}>
             Besoin d&apos;un renseignement ?
-          </button>
+          </Link>
         </div>
       </div>
     </section>
