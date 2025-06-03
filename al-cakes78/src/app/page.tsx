7fc7382472fd94861be1dd9d676d0120817763c4
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-06-03 14:38:23 +0200

    refactor(styles): clean and restructure global layout and Hero section

--- al-cakes78/src/app/page.tsx
+++ al-cakes78/src/app/page.tsx
@@ -1,11 +1,19 @@
 import Hero from "@/components/Hero/Hero";
 import CardInfos from "@/components/CardInfos/CardInfos";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <CardInfos />
+      <Image
+        src="/images/vector.svg"
+        width={594}
+        height={100}
+        alt="Image de séparation"
+        className="vector-image"
+      />
     </>
   );
 }
