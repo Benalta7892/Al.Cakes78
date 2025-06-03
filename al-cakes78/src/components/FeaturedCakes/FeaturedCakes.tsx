53e7a709a075b01321ce42caf064b49556ca115a
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-06-03 19:32:54 +0200

    feat: add featured cakes carousel with Embla

--- al-cakes78/src/components/FeaturedCakes/FeaturedCakes.tsx
+++ al-cakes78/src/components/FeaturedCakes/FeaturedCakes.tsx
@@ -1,13 +1,16 @@
 import styles from "./FeaturedCakes.module.css";
 import { CAKES } from "@/data/cakes";
 import FeaturedCakesCarousel from "./FeaturedCakesCarousel";
+import { EmblaOptionsType } from "embla-carousel";
+// import Link from "next/link";
 
 const FeaturedCakes = () => {
   const featuredCakes = CAKES.filter((cake) => cake.isFeatured);
+  const OPTIONS: EmblaOptionsType = { loop: true };
 
   return (
     <section className={styles["featured-cakes-section"]}>
-      <FeaturedCakesCarousel cakes={featuredCakes} />
+      <FeaturedCakesCarousel cakes={featuredCakes} options={OPTIONS} />
     </section>
   );
 };
