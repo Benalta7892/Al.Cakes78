7ed88750de4f2f1b6e6364d937e6c23bd2c9c3a8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-06-04 02:30:39 +0200

    feat(featured-cakes): improve layout, style and content with reusable CakeCard, updated styles and assets

---
+++ al-cakes78/src/components/CakeCard/CakeCard.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./CakeCard.module.css";
+
+type Props = {
+  id: number;
+  name: string;
+  slug: string;
+  image: string;
+  description: string;
+  price: number;
+};
+
+const CakeCard = ({ name, slug, image, description, price }: Props) => {
+  return (
+    <div className={styles["cake-card-wrapper"]}>
+      <Link
+        href={`/cakes/${slug}`}
+        className={`${styles["cake-card-link"]} bordered cursor-pointer hover:shadow-lg hover:scale-[1.01] transition duration-200`}>
+        <div className={`${styles["cake-card"]} bordered`}>
+          <h3 className={styles["cake-card-title"]}>{name}</h3>
+          <div className={styles["cake-card-image-wrapper"]}>
+            <Image
+              className={styles["cake-card-image"]}
+              src={image}
+              alt={name}
+              height={280}
+              width={280}
+              style={{ filter: "drop-shadow(2px 4px 20px rgba(0, 0, 0, 0.5))" }}
+            />
+          </div>
+          <div className={styles["cake-card-content"]}>
+            <p className={styles["cake-card-description"]}>{description}</p>
+            <p className={styles["cake-card-price"]}>
+              À partir de {price.toFixed(2)} €
+            </p>
+            {/* <button className={styles["cake-card-button"]}>Découvir</button> */}
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default CakeCard;
