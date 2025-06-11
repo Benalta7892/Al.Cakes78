236f7284956036e2b347017fc2cf178bd0d941f4
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-06-11 15:33:56 +0200

    refactor: extract ingredients and molds into separate components for clarity

--- al-cakes78/src/components/CakeDetails/CakeInfosDisplay.tsx
+++ al-cakes78/src/components/CakeDetails/CakeInfosDisplay.tsx
@@ -1,10 +1,44 @@
 import styles from "./CakeInfosDisplay.module.css";
+import Image from "next/image";
+import IntroSection from "@/components/IntroSection/IntroSection";
+import CakeIngredientsSection from "./CakeIngredientsSection";
+import CakeMoldsSection from "./CakeMoldsSection";
 
 const CakeInfosDisplay = () => {
   return (
     <div className={`${styles["display-container"]} bordered`}>
       <div className={`${styles["display-wrapper"]} bordered`}>
-        (Cake Infos Display...)
+        {/* Logo centré */}
+        <div className={styles["logo-container"]}>
+          <Image
+            src="/images/Logo.svg"
+            width={264}
+            height={237}
+            alt="Logo"
+            className={styles["img"]}
+          />
+        </div>
+
+        {/* Titre h2 */}
+        {/* Description */}
+        <IntroSection
+          title="Dalia, le Gâteau Fruité"
+          description={
+            <>
+              Dalia est un entremets fruité, léger et gourmand. Sa génoise à la
+              vanille est garnie d&apos;une mousse légère aux fruits rouges, le
+              tout recouvert d&apos;un glaçage miroir. Décoré avec des fruits
+              frais et des fleurs comestibles, il est parfait pour toutes les
+              occasions.
+            </>
+          }
+        />
+
+        {/* Ingrédients avec un titre et une liste */}
+        <CakeIngredientsSection />
+
+        {/* Moules disponible (taille et prix) avec un titre et un tableau */}
+        <CakeMoldsSection />
       </div>
     </div>
   );
---
+++ al-cakes78/src/components/CakeDetails/CakeIngredientsSection.tsx
@@ -0,0 +1,16 @@
+const CakeIngredients = () => {
+  return (
+    <section>
+      <h3>Ingrédients</h3>
+      <ul>
+        <li>Génoise à la vanille</li>
+        <li>Mousse légère aux fruits rouges</li>
+        <li>Glaçage miroir</li>
+        <li>Décor en chocolat blanc</li>
+        <li>Fruits frais</li>
+        <li>Fleurs comestibles</li>
+      </ul>
+    </section>
+  );
+};
+export default CakeIngredients;
