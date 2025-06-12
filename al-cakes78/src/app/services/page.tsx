87c8f5f8215df9704a2d4be8f9b9b42329e1c573
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-06-12 20:22:19 +0200

    feat: implement Services section layout and improve display styles

--- al-cakes78/src/app/services/page.tsx
+++ al-cakes78/src/app/services/page.tsx
@@ -1,4 +1,38 @@
+import ContactCTA from "@/components/ContactCTA/ContactCTA";
+import ServicesSection from "@/components/ServicesSection/ServicesSection";
+import Image from "next/image";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Image
+        src="/images/vector.svg"
+        width={594}
+        height={100}
+        alt="Image de séparation"
+        className="vector-image"
+      />
+
+      <ServicesSection />
+
+      <Image
+        src="/images/vector.svg"
+        width={594}
+        height={100}
+        alt="Image de séparation"
+        className="vector-image"
+      />
+
+      <ContactCTA />
+
+      <Image
+        src="/images/vector.svg"
+        width={594}
+        height={100}
+        alt="Image de séparation"
+        className="vector-image"
+      />
+    </>
+  );
 };
 export default page;
