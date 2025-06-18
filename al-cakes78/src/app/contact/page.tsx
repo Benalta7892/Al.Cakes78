5b474b09549c6f34a0712834f4259abe93e22155
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-06-18 12:10:34 +0200

    feat(contact): implement step-based form start page

--- al-cakes78/src/app/contact/page.tsx
+++ al-cakes78/src/app/contact/page.tsx
@@ -1,4 +1,27 @@
+import Image from "next/image";
+import FormStart from "./FormStart/FormStart";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <Image
+        src="/images/vector.svg"
+        width={594}
+        height={100}
+        alt="Image de séparation"
+        className="vector-image"
+      />
+
+      <FormStart />
+
+      <Image
+        src="/images/vector.svg"
+        width={594}
+        height={100}
+        alt="Image de séparation"
+        className="vector-image"
+      />
+    </>
+  );
 };
 export default page;
