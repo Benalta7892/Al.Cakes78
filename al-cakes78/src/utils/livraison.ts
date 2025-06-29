342814f02f2d8b8409b2bd66345e75f7e5647fac
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2025-06-29 16:39:27 +0200

    feat(contact): display delivery zone info and pricing

---
+++ al-cakes78/src/utils/livraison.ts
@@ -0,0 +1,33 @@
+import { DELIVERY_ZONES } from "@/data/deliveryZones";
+
+export const getDeliveryZone = (codePostal: string, totalPrice: number) => {
+  if (totalPrice >= 85) {
+    return {
+      zone: "",
+      price: 0,
+      description: "",
+    };
+  }
+
+  if (DELIVERY_ZONES.zone1.includes(codePostal)) {
+    return {
+      zone: "Zone 1",
+      price: 10,
+      description: "≤ 15 km",
+    };
+  }
+
+  if (DELIVERY_ZONES.zone2.includes(codePostal)) {
+    return {
+      zone: "Zone 2",
+      price: 15,
+      description: "≤ 30 km",
+    };
+  }
+
+  return {
+    zone: "Zone 3",
+    price: 20,
+    description: "> 30 km",
+  };
+};
